21e5e9c287dc7d4feed8d8cbc431a269b5126c83
onetwothreedotcom <pedrinaraymond@gmail.com>
2026-04-01 01:42:00 +0800

    Added Initial HashHelper

--- Finals_Q1_Backend_BSIT32E2_PEDRINA_RAYMOND/Helpers/HashHelper.cs
+++ Finals_Q1_Backend_BSIT32E2_PEDRINA_RAYMOND/Helpers/HashHelper.cs
@@ -1,6 +1,30 @@
-﻿namespace Finals_Q1_Backend_BSIT32E2_PEDRINA_RAYMOND.Helpers
+﻿using System.Security.Cryptography;
+using System.Text;
+using Finals_Q1_Backend_BSIT32E2_PEDRINA_RAYMOND.Models;
+
+namespace Finals_Q1_Backend_BSIT32E2_PEDRINA_RAYMOND.Helpers
 {
-    public class HashHelper
+    public static class HashHelper
     {
+        public static string ComputeHash(Todo todo, string previousHash)
+        {
+            var rawData =
+                $"{todo.Id}|{todo.Title}|{todo.Completed}|{todo.CreatedAt:O}|{previousHash}";
+
+            using var sha256 = SHA256.Create();
+            var bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+
+            return Convert.ToHexString(bytes);
+        }
+
+        public static string ComputeProof(string title, int nonce)
+        {
+            var rawData = $"{title}|{nonce}";
+
+            using var sha256 = SHA256.Create();
+            var bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+
+            return Convert.ToHexString(bytes);
+        }
     }
 }
